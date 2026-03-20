9062f0b6a46b1fb49dddf3d2834fccfdf0ac423a
gordondiair <dorian.girod@gmail.com>
2026-03-20 09:37:37 +0100

    Rajout de la possibilité d'importer les fonctions du fichier "outils_eda" dans vos notebook avec les explications en haut de celui-ci

--- CODE_ZONE/dorian_code/outils_eda.py
+++ CODE_ZONE/dorian_code/outils_eda.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+### créé un histogramme pour le dataframe "df" et la colonne que vous souhaitez, attention, fonctionne seulement si vous utilisez le df qui vient de "main_cleaned.parquet" car la fonction récupère "year"
 def histogram(df, column: str):
     try:
         df.hist(bins = 20, column = column)
@@ -10,6 +12,7 @@ def histogram(df, column: str):
     except : 
         print("Attention de travailler uniquement sur le fichier main_cleaned.parquet")
 
+### créé un graphique d'évolution dans le temps de la variable column qui se trouve dans le dataframe "df"
 def time_evo (df,column: str):
     try:
         plt.plot(df['year'], df[column])
