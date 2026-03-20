606763db5b3f3f2ede8cfdeb52e83940bc1a91e6
gordondiair <dorian.girod@gmail.com>
2026-03-20 10:44:37 +0100

    Création de la fonction heatmap

--- CODE_ZONE/dorian_code/outils_eda.py
+++ CODE_ZONE/dorian_code/outils_eda.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt 
+import seaborn as sns
 
 
 ### créé un histogramme pour le dataframe "df" et la colonne que vous souhaitez, attention, fonctionne seulement si vous utilisez le df qui vient de "main_cleaned.parquet" car la fonction récupère "year"
@@ -13,7 +14,7 @@ def histogram(df, column: str):
         plt.show()
         plt.close()
     except Exception as e: 
-        print(f"Erreur : {e}")
+        print(f"Erreur dans l'histogramme: {e}")
 
 ### créé un graphique d'évolution dans le temps de la variable column qui se trouve dans le dataframe "df"
 def time_evo (df,column: str):
@@ -25,18 +26,36 @@ def time_evo (df,column: str):
         plt.show()
         plt.close()
     except Exception as e: 
-        print(f"Erreur : {e}")
+        print(f"Erreur dans le graphique d'évolution: {e}")
 
 ### créé un scatter plot pour les variables x et y du dataframe "df"
 def scat (df,x: str,y: str):
     try:
         plt.scatter(df[x],df[y])
+        plt.ticklabel_format(style='plain', axis='y')
         plt.xlabel(x)
         plt.ylabel(y)
         plt.show()
         plt.close()
     except Exception as e:
-        print(f"Erreur: {e}")
+        print(f"Erreur dans le scatter plot: {e}")
+
+def corr_matrix(df):
+    try:
+        corr = df.corr(numeric_only=True) 
+        
+        plt.figure(figsize=(12, 10))
+        sns.heatmap(corr, 
+                    annot=True,
+                    fmt=".2f",
+                    cmap='coolwarm',
+                    vmin=-1, vmax=1,
+                    center=0,
+                    linewidths=.5)
+        plt.title("Matrice de Corrélation")
+        plt.show()
+    except Exception as e:
+        print(f"Erreur dans matrice de corrélation : {e}")
     
 if __name__ == "__main__":
     print("Vous êtes bien dans le fichier outils_eda")
